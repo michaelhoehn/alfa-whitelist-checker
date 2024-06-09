9eea699f334fbd5e6c1ef57da0127b9957d8d9a1
Michael Hoehn <michael.hoehn@burohappold.com>
2024-06-08 22:59:34 -0400

    smol fixes

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -5,17 +5,11 @@ import { NEXT_PUBLIC_URL } from '../../config';
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const body: FrameRequest = await req.json();
 
-  // Log the incoming request body
-  // console.log('Request body:', JSON.stringify(body));
-
   try {
     const { isValid, message } = await getFrameMessage(body, {
       neynarApiKey: process.env.NEXT_PUBLIC_NEYNAR_API_KEY,
       // allowFramegear: true, // Enable Framegear for testing
     });
-
-    // Log the response from getFrameMessage
-    // console.log('Frame message response:', JSON.stringify({ isValid, message }));
 
     if (!isValid) {
       return new NextResponse('Message not valid', { status: 500 });
@@ -25,13 +19,14 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     const channelAddress = '0x8a2e9ee84aaaa7f542accb937130fffab4762668';
 
     if (message?.button === 1) {
-      // Check status button
       const response = await fetch(
         `https://alfafrens.com/api/v0/isUserByFidSubscribedToChannel?channelAddress=${channelAddress}&fid=${fid}`,
       );
       const data = await response.json();
 
-      if (data.isSubscribed) {
+      // console.log('API response:', JSON.stringify(data));
+
+      if (data) {
         return new NextResponse(
           getFrameHtmlResponse({
             buttons: [
@@ -44,6 +39,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
             ],
             image: {
               src: `${NEXT_PUBLIC_URL}/verified.png`,
+              aspectRatio: '1:1',
             },
             postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
           }),
@@ -61,6 +57,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
             ],
             image: {
               src: `${NEXT_PUBLIC_URL}/not_verified.png`,
+              aspectRatio: '1:1',
             },
             postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
           }),
@@ -79,6 +76,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         ],
         image: {
           src: `${NEXT_PUBLIC_URL}/not_verified.png`,
+          aspectRatio: '1:1',
         },
         postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
       }),
